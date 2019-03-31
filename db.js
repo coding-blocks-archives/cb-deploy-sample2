5b6246347ca2046248097cdb42e0684a3d64ea55
Arnav Gupta <arnav@codingblocks.com>
2019-03-31 17:12:14 +0530

    add database connection
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ db.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+
+const username = "sampleuser1"
+const password = "samplepass1"
+const host = "localhost"
+const port = "3306"
+const dbname = "sampledb1"
+
+const DB_URL = process.env.CLEARDB_DATABASE_URL ||
+  `mysql://${username}:${password}@${host}:${port}/${dbname}`
+
+const db = new Sequelize(DB_URL)
+
+const Tasks = db.define('task', {
+  name: Sequelize.STRING,
+  done: Sequelize.BOOLEAN
+})
+
+module.exports = {
+  db, Tasks
+}
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const app = require('express')()
+const {
+  db,
+  Tasks
+} = require('./db')
 
-const PORT = process.env.PORT  || 4444
+const PORT = process.env.PORT || 4444
 
 app.get('/', (req, res) => res.send('Hello!'))
 
-app.listen(PORT, () => {
-  console.log(`Started on http://localhost:${PORT}`)
+app.get('/tasks', async (req, res) => {
+  res.send(await Tasks.findAll())
 })
+
+app.get('/tasks/add', async (req, res) => {
+  await Tasks.create({
+    name: req.query.name,
+    done: req.query.done == 'true'
+  })
+  res.redirect('/tasks')
+})
+
+db.sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Started on http://localhost:${PORT}`)
+    })
+
+  })
+  .catch(console.error)
